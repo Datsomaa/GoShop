0f757acf4a38bf9659a562c251f6931275124a34
Canh1309 <phamduccanh1309@gmail.com>
2024-12-26 22:29:04 +0700

    Delete canh directory

--- canh/js/login.js
+++
@@ -1,7 +0,0 @@
-// Hiển thị hoặc ẩn mật khẩu
-document.getElementById("showPassword").addEventListener("change", () => {
-  const password = document.getElementById("password");
-  const isChecked = document.getElementById("showPassword").checked;
-  const type = isChecked ? "text" : "password";
-  password.setAttribute("type", type);
-});
